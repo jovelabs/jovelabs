634652b38f7c8ef5eed7d96c3a82f9793406f5e2
zpatten <zpatten@jovelabs.com>
2013-01-27 19:38:06 -0800

    add container

--- lib/jove_labs/container.rb
+++ lib/jove_labs/container.rb
@@ -1,4 +1,15 @@
 module JoveLabs
-  class Container
+
+  class Container < ZTK::DSL::Base
+    belongs_to :environment, :class_name => "JoveLabs::Environment"
+    belongs_to :network, :class_name => "JoveLabs::Network"
+
+    attribute :hostname
+    attribute :ip
+    attribute :mac
+    attribute :distro
+    attribute :arch
+    attribute :persist
   end
+
 end
