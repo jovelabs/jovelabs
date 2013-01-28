d042b2ae9c8be6cb99efaac7aeb9743dc59d77e4
zpatten <zpatten@jovelabs.com>
2013-01-27 19:37:46 -0800

    use ztk DSL; active support inflector; autoload our classes

--- lib/jovelabs.rb
+++ lib/jovelabs.rb
@@ -1,5 +1,25 @@
+require "ztk"
+require "ztk/dsl"
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.uncountable 'nexus'
+end
+
+require "jove_labs/support"
 require "jove_labs/version"
 
 module JoveLabs
-  # Your code goes here...
+  extend(JoveLabs::Support)
+
+  class Error < StandardError; end
+
+  autoload :Nexus, "jove_labs/nexus"
+
+  autoload :Environment, "jove_labs/environment"
+
+  autoload :Container, "jove_labs/container"
+  autoload :Network, "jove_labs/network"
+
+  autoload :Provider, "jove_labs/provider"
+  autoload :Providers, "jove_labs/providers"
 end
