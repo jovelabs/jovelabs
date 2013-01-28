4cf95d86e8b63d735a486f85f3ce7dded3310c2c
zpatten <zpatten@jovelabs.com>
2013-01-27 19:43:46 -0800

    update copyright

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 ################################################################################
 #
 #      Author: Zachary Patten <zachary@jovelabs.net>
-#   Copyright: Copyright (c) Jove Labs
+#   Copyright: Copyright (c) 2012-2013 Jove Labs
 #     License: Apache License, Version 2.0
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
